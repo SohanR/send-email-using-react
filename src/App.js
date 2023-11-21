c5ea3985f78a934ec34110f26865f88cd23a16c7
sohanr <mizan.rahman66d@gmail.com>
2023-11-22 01:34:21 +0600

    email send

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+import GithubCorner from 'react-github-corner';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
+
+
+
 function App() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+          'service_floapca', 
+          'template_619ha4c', 
+          form.current, 
+          '1lddeiOY-rFCWLv94'
+      )
+      .then((result) => {
+          toast.success('Email sent, Thank you!');
+      }, (error) => {
+          toast.error('Something went wrong,try again!');
+      });
+
+      e.target.reset();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GithubCorner href="https://github.com/SohanR/send-email-using-react" /> 
+
+      <h1 className='title'>Send Email Using ReactJS without Backend</h1> 
+
+
+      <div className='container border'>
+        <form className='row' style={{margin:"25px 85px 75px 100px"}} ref={form} onSubmit={sendEmail} >
+            
+            <label className='tag'>Name</label>
+            <input className='form-control' type="text" name='name'  placeholder='' required/>
+            
+
+           
+            <label className='tag'>Email</label>
+            <input className='form-control' type="email" name='email'  placeholder='' required/>
+        
+
+        
+            <label className='tag'>Message</label>
+            <textarea className='form-control' name="message"  cols="25" rows="4"  placeholder='' required></textarea>
+
+            <input className='form-control btn btn-primary' type="submit" value='send' style={{marginTop:'30px'}}/>
+        </form>
+      </div>
+
+      <ToastContainer
+            position="top-center"
+            autoClose={1000}
+            hideProgressBar
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable={false}
+            pauseOnHover={false}
+            theme="dark"
+        />
     </div>
   );
 }
